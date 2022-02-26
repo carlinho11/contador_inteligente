0951741645343b8e805fd3c6cd9aa569b3de37b2
Carlinho11 <carlosrobertodosreis11@gmail.com>
2022-02-26 16:20:14 -0300

    -

--- lib/app/shared/ui/app_ui.dart
+++ lib/app/shared/ui/app_ui.dart
@@ -7,10 +7,4 @@ class AppUi {
   static const Color corSecundaria = Color(0xffeba834);
   static const Color corFundo = Color(0xFFF2F2F2);
   static const Color corPreto = Color(0xFFF2F2F2);
-
-  static const BoxShadow sombraPrincipal = BoxShadow(
-    color: Color.fromRGBO(0, 0, 0, 0.1),
-    blurRadius: 20,
-    offset: Offset(0, 4),
-  );
 }
