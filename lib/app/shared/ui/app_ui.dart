880e9a9dcedfa343ecc0b62e5d90e561242a3107
Carlinho11 <carlosrobertodosreis11@gmail.com>
2022-02-26 16:21:20 -0300

    .

--- lib/app/shared/ui/app_ui.dart
+++ lib/app/shared/ui/app_ui.dart
@@ -6,5 +6,4 @@ class AppUi {
   static const Color corPrincipal = Color(0xFF234d70);
   static const Color corSecundaria = Color(0xffeba834);
   static const Color corFundo = Color(0xFFF2F2F2);
-  static const Color corPreto = Color(0xFFF2F2F2);
 }
